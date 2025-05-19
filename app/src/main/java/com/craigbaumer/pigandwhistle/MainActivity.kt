2ed6330b85d08e39647f16c716fa22096925c4da
Craig <craig.baumer@gmail.com>
2025-05-19 16:35:53 -0400

    Small updates

--- app/src/main/java/com/craigbaumer/pigandwhistle/MainActivity.kt
+++ app/src/main/java/com/craigbaumer/pigandwhistle/MainActivity.kt
@@ -11,8 +11,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.craigbaumer.pigandwhistle.ui.Trains
 import com.craigbaumer.pigandwhistle.ui.theme.PigAndWhistleTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,7 +23,6 @@ class MainActivity : ComponentActivity() {
 
     val viewModel: MainViewModel by viewModels()
 
-    @SuppressLint("StateFlowValueCalledInComposition")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel.getTrainLocations() // TODO
@@ -30,9 +31,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             PigAndWhistleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Hello Pig and Whistle!",
-                        modifier = Modifier.padding(innerPadding)
+                    Trains(
+                        modifier = Modifier.padding(innerPadding),
+                        trainLocations = viewModel.state.collectAsState().value,
                     )
                 }
             }
--- app/src/main/java/com/craigbaumer/pigandwhistle/ui/Trains.kt
+++ app/src/main/java/com/craigbaumer/pigandwhistle/ui/Trains.kt
@@ -3,24 +3,32 @@ package com.craigbaumer.pigandwhistle.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import com.craigbaumer.pigandwhistle.ui.theme.PigAndWhistleTheme
+import com.craigbaumer.septa.data.TrainLocation
+import com.craigbaumer.septa.data.TrainLocations
 
 @Composable
 fun Trains(
-    trains: List<String>
+    modifier: Modifier = Modifier,
+    trainLocations: TrainLocations
 ) {
-    PigAndWhistleTheme {
-        LazyColumn(
-            modifier = Modifier.background(color = MaterialTheme.colorScheme.primaryContainer)
-        ) {
-            items(trains) { train ->
+    LazyColumn(
+        modifier = modifier.background(color = MaterialTheme.colorScheme.primaryContainer)
+    ) {
+        items(trainLocations.trainLocations) { train ->
+            Card(
+                elevation = CardDefaults.elevatedCardElevation(),
+            ) {
                 Text(
-                    text = train,
+                    text = train.nextStopName,
                     color = MaterialTheme.colorScheme.onPrimaryContainer,
                 )
             }
@@ -31,5 +39,31 @@ fun Trains(
 @PreviewLightDark
 @Composable
 fun TrainsPreview() {
-    Trains(listOf("M1", "M1"))
+    PigAndWhistleTheme {
+        Trains(
+            trainLocations = TrainLocations(
+                trainLocations = listOf(
+                    TrainLocation(
+                        lat = "40.050251",
+                        lng = "-75.347250",
+                        label = "136",
+                        routeId = "M1",
+                        trip = "71328",
+                        vehicleID = "136",
+                        blockID = "4019",
+                        direction = "N/A",
+                        destination = "69th Street Transit Center",
+                        late = 2,
+                        nextStopId = "32192",
+                        nextStopName = "Radnor - NHSL",
+                        nextStopSequence = 7,
+                        estimatedSeatAvailability = "NOT_AVAILABLE",
+                        offset = 2,
+                        offsetSec = "102",
+                        timestamp = 1747685589,
+                    ),
+                ),
+            ),
+        )
+    }
 }
